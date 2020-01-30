c4dc3a1c936308cd3cc82b1f44f39d6f64778412
MBNV124 <60343462+MBNV124@users.noreply.github.com>
2020-01-30 12:24:13 +0200

    Updated

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+    session_start();
+
+    if (!isset($_SESSION['fruitList'])) {
+        $_SESSION['fruitList'] = array("green", "tomates");
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Plodovete</title>
+    <link rel="stylesheet" href="style.css">    
+</head>
+<body>   
+    <div class="container">
+        <header>
+            <h1 class="heading">plodove</h1>
+            <h2 class="heading">fresh plodove</h2>
+        </header>
+
+    <?php
+        print_r($_SESSION);
+    ?>
+    </div>
+</body>
+</html>
+ 
